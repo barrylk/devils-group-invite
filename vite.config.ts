f20c3c3f2617453b7e60b113582ec35dc301a57a
barrylk <114392920+barrylk@users.noreply.github.com>
2025-04-23 13:13:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './', // Ensure this points to the root of your project
   plugins: [react()],
   server: {
     port: 5173
